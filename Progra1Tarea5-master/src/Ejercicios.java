bdd195475f414edde1438f133f289d7a15df5c10
MaynorCruz <Maynorcruz@unitec.edu>
2014-09-22 08:01:33 -0600

    cheke

--- Progra1Tarea5-master/src/Ejercicios.java
+++ Progra1Tarea5-master/src/Ejercicios.java
@@ -1,7 +1,8 @@
 
 public class Ejercicios
 {
-	
+	// Creamos el arreglo que vuelva la posicion de la columna
+	// En el mismo arreglo creamos la posici0n que devuelva la fila
 	//Devuelve el valor de mi_arreglo en la posicion [columna][fila]
 	static String obtenerValor(String mi_arreglo[][], int columna, int fila)
 	{
@@ -9,18 +10,23 @@ public class Ejercicios
 	}
 	
 	//Devuelve la cantidad de columnas de mi_arreglo
+	//Creamos el arreglo dentro del return para devolver lo que se nos pide 
 	static int obtenerCantidadColumnas(double mi_arreglo[][])
 	{
 		return mi_arreglo.length;
 	}
 	
 	//Devuelve la cantidad de filas de mi_arreglo
+	// Creamos el mismo arreglo para devolver la cantidad de filas que se nos pide
 	static int obtenerCantidadFilas(double mi_arreglo[][])
 	{
 		return mi_arreglo[1].length;
 	}
 	
 	//Devuelve la cantidad de elementos que puede alacenar mi_arreglo
+	// Primero creamos la suma
+	// Agregamos un ciclo for para para la columnas y las sumas
+	// En return cramos suma.
 	static int obtenerCantidadElementos(double mi_arreglo[][])
 	{
 		double suma = 0;
@@ -34,6 +40,9 @@ public class Ejercicios
 	}
 	
 	//Devuelve la sua de todos los elementos de mi_arreglo
+	// Declaramos la sumas con valor inicial de cero
+	// Iniciamos en cilco for para las filas y suma
+	// Devuelve la suma en return
 	static double sumaElementos(double mi_arreglo[][])
 	{
 		double suma = 0;
@@ -50,6 +59,9 @@ public class Ejercicios
 	}
 	
 	//Devuelve el promedio de todos los elementos de mi_arreglo
+	// Creamos la suma y el promedio
+	// Inicamos el ciclo for para las filas y columnas
+	// Devuelve la sumas y el promedio en return
 	static double promedioElementos(double mi_arreglo[][])
 	{
 		double suma = 0;
@@ -69,6 +81,9 @@ public class Ejercicios
 	}
 	
 	//Devuelve verdadero si num esta en mi_arreglo, de lo contrario devuelve false
+	// Iniciamos con el ciclo for para las columnas
+	// HAcemos otro ciclo for para las filas
+	// Creamos la sentencia 
 	static boolean existe(double mi_arreglo[][], int num)
 	{
 		for(int columna = 0; columna < mi_arreglo.length; columna++)
